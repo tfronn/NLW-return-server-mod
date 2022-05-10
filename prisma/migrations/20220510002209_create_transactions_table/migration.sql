e3656a9894d9bca4e3f10d72331e20df78b5097a
Tfronn <keeelvincamilo@hotmail.com>
2022-05-09 22:25:46 -0300

    alterações db 5

--- prisma/migrations/20220510002209_create_transactions_table/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "transactions" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "amount" INTEGER NOT NULL,
-    "category" TEXT NOT NULL,
-    "type" TEXT NOT NULL
-);
