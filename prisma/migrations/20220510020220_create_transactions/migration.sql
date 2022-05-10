1b73f163bb971c896310c09eda9f4210d12daeb6
Tfronn <keeelvincamilo@hotmail.com>
2022-05-09 23:03:22 -0300

    teste

--- prisma/migrations/20220510020220_create_transactions/migration.sql
+++ prisma/migrations/20220510020220_create_transactions/migration.sql
@@ -1,17 +1,21 @@
 -- CreateTable
 CREATE TABLE "feedbacks" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "type" TEXT NOT NULL,
     "comment" TEXT NOT NULL,
-    "screenshot" TEXT
+    "screenshot" TEXT,
+
+    CONSTRAINT "feedbacks_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "transactions" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "amount" INTEGER NOT NULL,
     "category" TEXT NOT NULL,
     "type" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
 );
