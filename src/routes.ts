4e800e744a86e5a0dd0ed4356bb4ab06fbf1c8f5
Tfronn <keeelvincamilo@hotmail.com>
2022-05-10 17:16:49 -0300

    alterações db 3

--- src/routes.ts
+++ src/routes.ts
@@ -30,7 +30,7 @@ routes.post('/transactions', async (req, res) => {
     createdAt
   })
 
-    return res.status(201).json(transactions).send();
+    return res.status(201).json([transactions]).send();
   } catch(err) {
     console.error(err)
 
